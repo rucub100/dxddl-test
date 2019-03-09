b2ddbdea6b6795156369fbc7519d4b9e6f305cb6
Ruslan Curbanov <ruslan.curbanov@uni-duesseldorf.de>
2019-03-09 20:24:45 +0100

    make the app usable with DXRAM

--- src/main/java/de/hhu/bsinfo/dxddl/test/App.java
+++ src/main/java/de/hhu/bsinfo/dxddl/test/App.java
@@ -3,12 +3,43 @@
  */
 package de.hhu.bsinfo.dxddl.test;
 
-public class App {
-    public String getGreeting() {
-        return "Hello world.";
+import java.util.Arrays;
+
+import de.hhu.bsinfo.dxram.app.AbstractApplication;
+import de.hhu.bsinfo.dxram.boot.BootService;
+import de.hhu.bsinfo.dxram.engine.DXRAMVersion;
+import de.hhu.bsinfo.dxutils.NodeID;
+
+public class App extends AbstractApplication {
+    @Override
+    public void main(String[] p_args) {
+        BootService bootService = getService(BootService.class);
+        
+        System.out.printf("\n");
+        System.out.printf(
+                "  Hello! I am %s running on node %s.\n",
+                getApplicationName(),
+                NodeID.toHexStringShort(bootService.getNodeID()));
+        System.out.printf("  My arguments are: %s\n", Arrays.toString(p_args));
+        System.out.printf("\n");
+        
+        // Put your application code running on the DXRAM node/peer here
     }
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+    @Override
+    public DXRAMVersion getBuiltAgainstVersion() {
+        return DXRAMVersion.fromString("0.8.0-SNAPSHOT");
+    }
+
+    @Override
+    public String getApplicationName() {
+        return "DXDDL Tests";
+    }
+
+    @Override
+    public void signalShutdown() {
+        // Interrupt any flow of your application and make sure it shuts down.
+        // Do not block here or wait for something to shut down. Shutting down of your application
+        // must be execute asynchronously
     }
 }
